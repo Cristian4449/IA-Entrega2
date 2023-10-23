76caf0f6c8b0da9853041d7d26591ad37a8a904e
Cristian4449 <camilo12345642@hotmail.com>
2023-10-22 19:40:20 -0500

    Update MulticapaFinal.py

--- MulticapaFinal.py
+++ MulticapaFinal.py
@@ -34,7 +34,7 @@ class MulticapaFinal:
         self.potencialActivacionOcultas=np.zeros((3,1))#potencial de activacion en las neuronas ocultas
         
         self.funcionActivacionOculta = np.zeros((3,1))
-       
+
         self.potencialActivacionSalidas=np.zeros((5,1))
         
         self.Y=np.zeros((5,1))#Potencial de activacion en la neurona de la salida
@@ -55,11 +55,12 @@ class MulticapaFinal:
     def softmax(self,x):
         valor_exponentes = np.exp(x - np.max(x))
         return valor_exponentes / np.sum(valor_exponentes, axis=1, keepdims=True)
+    
     def propagacion(self,entradas):
         #Capa oculta
         self.potencialActivacionOcultas =np.dot(entradas,self.pesoOcultas)+self.umbralNeuronasOcultas
         self.funcionActivacionOculta =self.sigmoid(self.potencialActivacionOcultas)
-        #capaSalida
+        #Capa salida
         self.potencialActivacionSalidas = np.matmul(self.funcionActivacionOculta, self.pesoSalidas.T) + self.umbralNeuronaSalida
         self.potencialSalida = self.potencialActivacionSalidas[0:1,:]
         
@@ -82,12 +83,8 @@ class MulticapaFinal:
         
         #actualizacion de pesos
         self.deltaSalida =np.transpose(self.deltaSalida)
-
         self.pesoSalidas += np.dot(self.deltaSalida, self.funcionActivacionOculta) * self.tasaAprendizaje
-
         self.umbralNeuronaSalida +=np.sum(self.deltaSalida,axis=0)*self.tasaAprendizaje
-        
-
         self.pesoOcultas += entradargb.dot(self.deltaNeuronasOcultas.T) * self.tasaAprendizaje
         self.umbralNeuronasOcultas += np.sum(self.deltaNeuronasOcultas,axis=0)*self.tasaAprendizaje
 
